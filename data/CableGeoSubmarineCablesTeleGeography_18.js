2db71ef101d00ee99a51c959612a8b7b712cdab5
polarresearchdesign <gudrun_eriksen@hotmail.com>
2025-04-22 06:41:21 +0200

    Add files via upload

---
+++ data/CableGeoSubmarineCablesTeleGeography_18.js
@@ -0,0 +1,1 @@
+var json_CableGeoSubmarineCablesTeleGeography_18 = {"type":"FeatureCollection","name":"CableGeoSubmarineCablesTeleGeography_18","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
