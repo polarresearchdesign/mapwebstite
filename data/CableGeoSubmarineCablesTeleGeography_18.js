c2a9de1d23275da731fc6e20cacef22f877bb0be
polarresearchdesign <gudrun_eriksen@hotmail.com>
2025-04-23 00:20:36 +0200

    Delete data directory

--- data/CableGeoSubmarineCablesTeleGeography_18.js
+++
@@ -1,1 +0,0 @@
-var json_CableGeoSubmarineCablesTeleGeography_18 = {"type":"FeatureCollection","name":"CableGeoSubmarineCablesTeleGeography_18","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
